c2ed8b4b57546cc42dfa7942b9a59cb6a122aa8e
yazidazfa <yasidafa@gmail.com>
2024-06-08 13:21:30 +0700

    update stock after buy item from checkout page

--- lib/ui/checkout_page.dart
+++ lib/ui/checkout_page.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kopma/bloc/user_bloc/user_bloc.dart';
 import 'package:kopma/data/model/item/item_model.dart';
-
 import '../bloc/detail_item_bloc/detail_item_bloc.dart';
+import '../data/datasource/network/firebase_item_datasource.dart';
 
 class CheckoutPage extends StatefulWidget {
   final ItemModel item;
@@ -19,16 +19,27 @@ class CheckoutPage extends StatefulWidget {
 class _CheckoutPageState extends State<CheckoutPage> {
   int _quantity = 1;
   late int _totalPrice;
+  late int _maxQuantity; // Store the maximum quantity available in Firebase
 
   @override
   void initState() {
     super.initState();
     _totalPrice = widget.item.price;
+    _fetchMaxQuantity(); // Fetch the maximum quantity from Firebase
+  }
+
+  Future<void> _fetchMaxQuantity() async {
+    try {
+      _maxQuantity = await FirebaseItemDataSource().getQuantity(widget.item.id!);
+      setState(() {});
+    } catch (e) {
+      print('Error fetching quantity from Firebase: $e');
+    }
   }
 
   void _incrementCounter() {
     setState(() {
-      if (_quantity < widget.item.quantity) {
+      if (_quantity < _maxQuantity) {
         _quantity += 1;
         _totalPrice = _quantity * widget.item.price;
       }
@@ -44,24 +55,35 @@ class _CheckoutPageState extends State<CheckoutPage> {
     });
   }
 
+  Future<void> _updateFirebaseQuantity() async {
+    try {
+      int currentQuantity = await FirebaseItemDataSource().getQuantity(widget.item.id!);
+      int updatedQuantity = currentQuantity - _quantity;
+      await FirebaseItemDataSource().updateQuantity(widget.item.id!, updatedQuantity);
+    } catch (e) {
+      print('Error updating quantity in Firebase: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Checkout'),
+        title: const Text('Checkout'),
       ),
       body: BlocListener<DetailItemBloc, DetailItemState>(
-        listener: (context, state) {
+        listener: (context, state) async {
           if (state is BuyItemFailure) {
             showOkAlertDialog(context: context, title: "Error", message: state.errorMessage);
           } else if (state is BuyItemSuccess) {
+            await _updateFirebaseQuantity(); // Update Firebase quantity after a successful purchase
             showOkAlertDialog(context: context, title: "Success", message: "Congrats! Your order is on its way!");
           }
         },
         child: BlocBuilder<UserBloc, UserState>(
           builder: (context, state) {
             return SingleChildScrollView(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
@@ -84,21 +106,21 @@ class _CheckoutPageState extends State<CheckoutPage> {
                                 children: [
                                   Text(
                                     widget.item.name,
-                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    style: const TextStyle(fontWeight: FontWeight.bold),
                                   ),
-                                  SizedBox(height: 8.0),
+                                  const SizedBox(height: 8.0),
                                   Text('Price: ${widget.item.price}'),
-                                  SizedBox(height: 8.0),
+                                  const SizedBox(height: 8.0),
                                   Row(
                                     children: [
                                       TextButton(
                                         onPressed: _decrementCounter,
-                                        child: Text("-"),
+                                        child: const Text("-"),
                                       ),
                                       Text('$_quantity'),
                                       TextButton(
                                         onPressed: _incrementCounter,
-                                        child: Text("+"),
+                                        child: const Text("+"),
                                       ),
                                     ],
                                   ),
@@ -110,7 +132,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 16.0),
+                  const SizedBox(height: 16.0),
                   Card(
                     elevation: 4.0,
                     child: Padding(
@@ -118,11 +140,11 @@ class _CheckoutPageState extends State<CheckoutPage> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          Text(
+                          const Text(
                             "Seller",
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          SizedBox(height: 8.0),
+                          const SizedBox(height: 8.0),
                           Text("Name: ${widget.item.sellerName ?? ""}"),
                           Text("Email: ${widget.item.sellerEmail ?? ""}"),
                           Text("Address: ${widget.item.sellerAddress ?? ""}"),
@@ -130,7 +152,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 16.0),
+                  const SizedBox(height: 16.0),
                   Card(
                     elevation: 4.0,
                     child: Padding(
@@ -141,14 +163,14 @@ class _CheckoutPageState extends State<CheckoutPage> {
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
+                              const Text(
                                 "Total Price",
                                 style: TextStyle(fontWeight: FontWeight.bold),
                               ),
-                              SizedBox(height: 8.0),
+                              const SizedBox(height: 8.0),
                               Text(
                                 "$_totalPrice",
-                                style: TextStyle(fontSize: 18.0),
+                                style: const TextStyle(fontSize: 18.0),
                               ),
                             ],
                           ),
@@ -159,7 +181,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                                 quantity: _quantity,
                               ));
                             },
-                            child: Text("Pay Now"),
+                            child: const Text("Pay Now"),
                           ),
                         ],
                       ),
